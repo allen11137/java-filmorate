40c68ea09364b5ec084082d59fa742aa42cf64de
allen11137 <a.zukkel@yandex.ru>
2023-08-02 00:08:33 +0200

    Revert "Update 25.07(1)"
    This reverts commit ee692dfc22de15432ca2638174166ecc03eaf563.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.util.HashSet;
