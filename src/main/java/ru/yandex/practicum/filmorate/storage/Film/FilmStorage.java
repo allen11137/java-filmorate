f1e34f73d859e03e9e3213859da992bf771c33d3
allen11137 <a.zukkel@yandex.ru>
2023-08-02 21:17:25 +0200

    02.08 Update

--- src/main/java/ru/yandex/practicum/filmorate/storage/Film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Film/FilmStorage.java
@@ -4,14 +4,9 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Map;
 
-
 public interface FilmStorage {
-
 	void addToFilm(Film film);
-
 	void updateFilm(Film film);
-
 	void deleteToFilm(Film film);
-
 	Map<Integer, Film> getListOfFilms();
 }
